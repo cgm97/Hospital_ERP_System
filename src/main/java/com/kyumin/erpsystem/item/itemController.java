d1d813b03bfbf929857451cba856a2e0f6a9e531
kyumin <cgm97@naver.com>
2021-04-06 16:06:17 +0900

    BE - Mapping anotation 변경
    @RequestMapping -> @GetMapping

--- src/main/java/com/kyumin/erpsystem/item/itemController.java
+++ src/main/java/com/kyumin/erpsystem/item/itemController.java
@@ -1,24 +1,24 @@
 package com.kyumin.erpsystem.item;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/item")
 public class itemController {
-	
-	@RequestMapping(value = "/use", method = RequestMethod.GET)
+
+	@GetMapping("/use")
 	public String use() {
 		return "/item/itemUse";
 	}
-	
-	@RequestMapping(value = "/machineList", method = RequestMethod.GET)
+
+	@GetMapping("/machineList")
 	public String machineList() {
 		return "/item/machine";
 	}
-	
-	@RequestMapping(value = "/totalList", method = RequestMethod.GET)
+
+	@GetMapping("/totalList")
 	public String totalList() {
 		return "/item/totalInventory";
 	}
--- src/main/java/com/kyumin/erpsystem/patient/PatientController.java
+++ src/main/java/com/kyumin/erpsystem/patient/PatientController.java
@@ -1,19 +1,19 @@
 package com.kyumin.erpsystem.patient;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/patient")
 public class PatientController {
 	
-	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	@GetMapping("/add")
 	public String add() {
 		return "/patient/patientAdd";
 	}
-	
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
+
+	@GetMapping("/list")
 	public String list() {
 		return "/patient/patientList";
 	}
